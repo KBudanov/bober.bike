ad9e532f3979ce09865822735f59c133c1aaf6d5
StVlad <v0971991179@gmail.com>
2025-03-20 13:03:25 +0200

    mobile

--- script.js
+++ script.js
@@ -29,3 +29,27 @@ let botUrl = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${tgId
     $.post(botUrl)
    // $.post(botUrl2)
 })
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
